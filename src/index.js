6878c9abc6901dd9e754b234d2fbdc99326011bc
Jaiswal <aayush.jaiswal@hpe.com>
2023-02-21 10:35:21 +0530

    v1.8

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import  { createRoot } from "react-dom/client";
 import { Widget } from "./Widget";
 
-const root = createRoot(document.getElementById("root"));
-root.render(<Widget />);
+export default Widget;
