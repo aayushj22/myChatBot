2aab5bb1ea5080e98c72467fcebf89757d2733cb
Jaiswal <aayush.jaiswal@hpe.com>
2023-02-21 12:04:00 +0530

    v1.1.1

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
 import { Widget } from "./src/Widget";
 
-export {
-  Widget
-}
-export default is-mychatbot;
+
+// eslint-disable-next-line import/no-anonymous-default-export
+const mychatbot = (props, element = null) => {
+  const load = () => {
+    if (element === null) {
+      const node = document.createElement("div");
+      node.setAttribute("id", "rasaWidget");
+      document.body.appendChild(node);
+    }
+    const mountElement = element || document.getElementById("rasaWidget");
+    const root = createRoot(mountElement);
+    root.render(<Widget {...props} />);
+  };
+
+  if (document.readyState === "complete") {
+    load();
+  } else {
+    window.addEventListener("load", () => {
+      load();
+    });
+  }
+};
+export default mychatbot;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import  { createRoot } from "react-dom/client";
 import { Widget } from "./Widget";
 
-export default Widget;
+const root = createRoot(document.getElementById("root"));
+root.render(<Widget />);
